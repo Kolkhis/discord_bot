27373bd1ad479da1c197d33162d1c154f63e13a6
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2023-12-27 22:19:04 -0500

    Initial code commit

---
+++ bot.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import os
+import discord
+import logging
+import wavelink
+from discord.ext import commands
+
+LAVALINK_PASS = os.environ['LAVALINK_PASS']
+
+class Bot(commands.Bot):
+    def __init__(self) -> None:
+        intents: discord.Intents = discord.Intents.default()
+        intents.message_content = True
+        super().__init__(command_prefix=":", intents=intents)
+        logging.basicConfig(filename="./bot.log", level=logging.INFO)
+        discord.utils.setup_logging(level=logging.INFO)
+
+    async def setup_hook(self) -> None:
+        nodes = [wavelink.Node(uri="http://0.0.0.0:2333", password=LAVALINK_PASS)]
+        await wavelink.Pool.connect(nodes=nodes, client=self, cache_capacity=None)
+
+    async def on_ready(self) -> None:
+        logging.info(f"Logged in: {self.user} | {self.user.id}")
+
+    async def on_wavelink_node_ready(self, payload: wavelink.NodeReadyEventPayload) -> None:
+        logging.info(f"Wavelink Node connected: {payload.node!r} | Resumed: {payload.resumed}")
+
+    async def on_wavelink_track_start(self, payload: wavelink.TrackStartEventPayload) -> None:
+        player: wavelink.Player | None = payload.player
+        if not player:
+            logging.warning('Track-start event received without a player.')
+            return
+
+        original: wavelink.Playable | None = payload.original
+        track: wavelink.Playable = payload.track
+
+        embed: discord.Embed = discord.Embed(title="Now Playing")
+
+        if track.uri and track.source:
+            embed.description = f"[*{track.title}* by **{track.author}**]({track.uri}) "\
+                                f"from **{track.source.title()}**"
+        else:
+            embed.description = f"*{track.title}* by **{track.author}**"
+
+        if track.artwork:
+            embed.set_image(url=track.artwork)
+
+        if original and original.recommended:
+            embed.description += f"\n\nThis track was recommended by {track.source.title()}."
+            logging.info(f"Track recommended:\nOriginal: {original} - {original!r}\n"
+                         f"Original Title: {original.title}")
+
+        if track.album.name:
+            embed.add_field(name="Album", value=track.album.name)
+
+        if track.preview_url:
+            embed.add_field(name="Link", value=track.preview_url)
+
+        logging.info(f"Track started: {track!r}")
+        await player.home.send(embed=embed)
+
---
+++ main.py
@@ -0,0 +1,220 @@
+import os
+import asyncio
+import discord
+import wavelink
+from typing import cast
+from discord.ext import commands
+from bot import Bot
+
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+bot: Bot = Bot()
+
+
+@bot.command(aliases=["p", "add", "a"])
+async def play(ctx: commands.Context, *, query: str) -> None:
+    """`:play (URL or search)` - Play song/playlist URL, or search for it\n"""
+    if not ctx.guild:
+        return
+    player: wavelink.Player
+    player = cast(wavelink.Player, ctx.voice_client)  # type: ignore
+    if not player:
+        try:
+            player = await ctx.author.voice.channel.connect(cls=wavelink.Player)  # type: ignore
+        except AttributeError:
+            await ctx.send("Please join a voice channel first before using this command.")
+            return
+        except discord.ClientException:
+            await ctx.send("I was unable to join this voice channel. Please try again.")
+            return
+
+    # enabled =  play songs and fetch recommendations
+    # partial =  play songs, but don't fetch recommendations
+    # disabled = do nothing
+    player.autoplay = wavelink.AutoPlayMode.enabled
+
+    # Lock the player to this channel
+    if not hasattr(player, "home"):
+        player.home = ctx.channel
+    elif player.home != ctx.channel:
+        await ctx.send(f"I'm already locked to {player.home.mention}.")
+        return
+
+    # fetch Tracks and Playlists
+    # If spotify is enabled (LavaSrc), fetch Spotify tracks if it's a URL
+    # use YouTube for regular searches (non-urls)
+    try:
+        tracks: wavelink.Search = await wavelink.Playable.search(query)
+    except wavelink.LavalinkLoadException as e:
+        if e.error == "The playlist does not exist.":
+            new_query = query.split("&")[0]
+            tracks: wavelink.Search = await wavelink.Playable.search(new_query)
+        else:
+            raise e
+
+    if not tracks:
+        await ctx.send(
+            f"Sorry {ctx.author.mention}, I couldn't find any tracks that match "
+            f'"{query}".\nTry again with a different query.'
+        )
+        return
+
+    if isinstance(tracks, wavelink.Playlist):
+        added: int = await player.queue.put_wait(tracks)
+        await ctx.send(f"Added the playlist **{tracks.name}** ({added} songs) to the queue.")
+    else:
+        track: wavelink.Playable = tracks[0]
+        await player.queue.put_wait(track)
+        await ctx.send(f"Added **`{track}`** to the queue.")
+
+    if not player.playing:
+        await player.play(player.queue.get(), volume=30)
+
+    if player and player.paused:
+        await player.pause(False)
+        await ctx.message.add_reaction("\u2705")
+
+    try:
+        await ctx.message.delete()
+    except discord.HTTPException:
+        pass
+
+
+@bot.command(aliases=["s", "sk", "next", "n"])
+async def skip(ctx: commands.Context) -> None:
+    """`:skip` - Skip the current song."""
+    player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
+    if not player:
+        return
+    await player.skip(force=True)
+    await ctx.message.add_reaction("\u2705")
+
+
+# @bot.command()
+# async def nightcore(ctx: commands.Context) -> None:
+#     """Set the filter to a nightcore style."""
+#     player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
+#     if not player:
+#         return
+#     filters: wavelink.Filters = player.filters
+#     filters.timescale.set(pitch=1.2, speed=1.2, rate=1)
+#     await player.set_filters(filters)
+#     await ctx.message.add_reaction("\u2705")
+
+
+@bot.command(name="toggle", aliases=["pause", "resume", "stop", "start", "t"])
+async def pause_resume(ctx: commands.Context) -> None:
+    """`:pause` / `:resume` - Pause or resume playback."""
+    player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
+    if not player:
+        return
+    await player.pause(not player.paused)
+    await ctx.message.add_reaction("\u2705")
+
+
+@bot.command(name="volume", aliases=["vol", "v"])
+async def volume(ctx: commands.Context, value: int | None = None) -> None:
+    """`:volume (0-50)` / `:vol` - Change the volume of the player."""
+    player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
+    if not player:
+        return
+    match value:
+        case None:
+            await ctx.send(f"The current volume is {player.volume}")
+        case value if value <= 50:
+            await player.set_volume(value)
+        case _:
+            await ctx.send("Enter a value between 0 and 50")
+        # await player.set_volume(value)
+    await ctx.message.add_reaction("\u2705")
+
+
+@bot.command(aliases=["dc", "quit", "exit", "q"])
+async def disconnect(ctx: commands.Context) -> None:
+    """`:quit` / `:dc` / `:exit` - Disconnect the player."""
+    player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
+    if not player:
+        return
+    await player.disconnect()
+    await ctx.message.add_reaction("\u2705")
+
+
+@bot.command(name="cmd", aliases=["commands", "cmds", "h", "man"])
+async def cmd(ctx: commands.Context) -> None:
+    """`:help` / `:h` - Get help: Display a list of commands."""
+    player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
+    author: str = ctx.author.name
+    embed: discord.Embed = discord.Embed(title="Help")
+    embed.description = f"""
+        SUP {author}, I'm a str8 up badass music bot. Here are my commands:\n
+        `:commands` / `:cmds` / `:h` - Display this help message\n
+        `:play (URL or search)` / `:add ...` - Play the song/playlist at the given URL, or search for it\n
+        `:volume (0-50)` - Change the volume of the player\n
+        `:pause`  - Pause the music player \n
+        `:resume` - Resume the music player\n
+        `:skip` - Skip the current song\n
+        `:autoplay (on/off)` - Toggle autoplay\n
+        `:queue` / `:vq` - Show the current queue\n
+    """
+    await ctx.message.add_reaction("🙃")
+    await ctx.send(embed=embed)
+    # await player.home.send(embed=embed)
+
+
+@bot.command(name="autoplay", aliases=["ap", "auto"])
+async def toggle_autoplay(ctx: commands.Context, value: str) -> None:
+    """`:autoplay (on/off)` / `:ap (on/off)` - Toggle autoplay."""
+    player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
+    if not player:
+        return
+    # enabled =  play songs and fetch recommendations
+    # partial =  play songs, but don't fetch recommendations
+    # disabled = do nothing
+    match value:
+        case "on":
+            player.autoplay = wavelink.AutoPlayMode.enabled
+        case "off":
+            player.autoplay = wavelink.AutoPlayMode.partial
+    await ctx.message.add_reaction("\u2705")
+
+
+# Add command: vq or q: view the queue
+@bot.command(name="queue", aliases=["vq", "list", "songs"])
+async def queue(ctx: commands.Context) -> None:
+    """`:queue` - View the current queue."""
+    player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
+    if not player:
+        await ctx.send("Not connected to a voice channel.")
+        return
+    if not player.queue:
+        await ctx.send("The queue is currently empty.")
+        return
+    # await ctx.send(f"The current queue:\n{player.queue}")
+    embed = discord.Embed(title="Current Queue", description=f"**{player.queue!r}**")
+    await ctx.send(embed=embed)
+
+@bot.command(name="state", aliases=["st", "status", "getstate"])
+async def get_state(ctx: commands.Context) -> None:
+    player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
+    if not player:
+        await ctx.send("Not connected to a voice channel.")
+        return
+    is_paused: bool = player.paused
+    playing_track: bool = player.playing
+    channel: str = player.channel.name
+    embed = discord.Embed(title="Current State")
+    embed.description = f"Paused:  {is_paused}\n"\
+                        f"Playing: {playing_track}\n"\
+                        f"Channel: {channel}"
+    await ctx.send(embed=embed)
+
+    
+        
+
+async def main() -> None:
+    async with bot:
+        await bot.start(BOT_TOKEN)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())

f74a7fb7e9680512db44f3f4f14c47832741c407
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2024-02-18 11:26:47 -0500

    feat: Add error handling and environment variable checks

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,53 @@
 #!/bin/bash
 
-. ./venv/bin/activate
-(cd ./Lavalink && java -jar ./Lavalink.jar) &
+check_vars() {
+    [[ -z "$XDG_CONFIG_HOME" ]] && XDG_CONFIG_HOME="$HOME/.config"
+    if [[ ! -s "$XDG_CONFIG_HOME/discord/OWNER_ID" ]]; then
+        printf "No Discord Owner ID provided.\n \
+            Please provide an Owner ID in %s\n" \
+            "$XDG_CONFIG_HOME/discord/OWNER_ID"
+    fi
+
+    if [[ ! -s "$XDG_CONFIG_HOME/discord/LAVALINK_PASS" ]]; then
+        printf "\nNo Lavalink Password provided.\n \
+            Please provide a Lavalink Password in %s\n\
+            It should be specified in application.yml." \
+            "$XDG_CONFIG_HOME/discord/LAVALINK_PASS"
+    fi
+
+    if [[ ! -s "$XDG_CONFIG_HOME/discord/BOT_TOKEN" ]]; then
+        printf "\nNo Discord Bot Token provided.\n \
+            Please provide a Discord Bot Token in %s\n" \
+            "$XDG_CONFIG_HOME/discord/BOT_TOKEN"
+    fi
+}
+
+
+if [[ ! -d "./venv" ]]; then
+    printf "No virtual environment found. Creating one...\n"
+    if ! python3 -m venv venv; then
+        printf "There was a problem creating the virtual environment.\n"
+        printf "Do you have %s and %s installed?\n" \
+        "python3-pip" \
+        "python3.10-venv (or later)"
+        exit 1
+    fi
+    cat >> ./venv/bin/activate <<- 'EOS'
+        BOT_TOKEN="$(head -1 "$HOME/.config/discord/BOT_TOKEN")"
+        LAVALINK_PASS="$(head -1 "$HOME/.config/discord/LAVALINK_PASS")"
+        OWNER_ID="$(head -1 "$HOME/.config/discord/OWNER_ID")"
+EOS
+fi
+
+check_vars
+
+if ! (cd ./Lavalink && java -jar ./Lavalink.jar) & 
+then
+    printf "There was a problem starting Lavalink!\n" && exit 1
+fi
 sleep 10;
-python3 -m kolbot 
+if ! python3 -m kolbot; then
+    printf "There was a problem starting kolbot!\n" && exit 1
+fi
+
 
